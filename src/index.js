651c8d0b36ca17af107f16b452fe0cfdd5254886
Ajay Singh Kunwar <ajaykunwar323@gmail.com>
2024-11-28 09:34:37 +0530

    connected to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+import 'dotenv/config';
+import connectDB from "./db/index.js";
+
+connectDB();
+/*
+import express from "express";
+const app = express();
+;(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${BD_NAME}`);
+        app.on("error",(error)=>{
+            console.log("error: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("error : ",error)
+        throw error
+    }
+})()
+*/
