361c64f3062e01f46daa37d334f2e1859ed3f073
Ajay Singh Kunwar <ajaykunwar323@gmail.com>
2024-12-14 16:08:26 +0530

    methods for token generation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-import {JsonWebTokenError} from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt';
 const userSchema= new Schema(
     {
@@ -55,5 +55,35 @@ const userSchema= new Schema(
     this.password=bcrypt.hash(this.password,10),
     next();
  })
+ userSchema.methods.isPasswordCorrect= async function(password){
+    return await bcrypt.compare(password,this.password);
+ }
+
+ userSchema.methods.generateAccessToken=  function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            username:this.username,
+            fullname : this.fullname,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECTRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+ }
+ userSchema.methods.generateRefreshToken=  function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,            
+        }
+    )
+ }
 
 export const User= mongoose.model("User",userSchema)
